0195b54a3fa45fc85094f5724484943247e57292
ranjana <sranjana182gmail.com>
2023-07-12 06:53:38 +0000

    added

---
+++ HelloServlet.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Hello, World!");
+    }
+}
+
